7d96a7edd6c5e2c2898e5036de8deb192613bed9
Tommy Parnell <tommy@terribledev.io>
2021-12-23 22:05:38 -0500

    init

---
+++ Readme.md
@@ -0,0 +1,1 @@
+An express app to show redirect to https in heroku
---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
+
+app.use((req, res, next) => {
+    const forwardedProto = req.headers["X-Forwarded-Proto"] || req.protocol
+    forwardedProto === 'https' ? next() : res.redirect('https://' + req.headers.host + req.url, 301)
+})
